483a883364147638af3bb6b66e6e5210e0cf96c1
Jakub Biesiada <biesiadajakub@icloud.com>
2018-12-22 11:54:17 +0100

    Fixes

--- src/helpers/checkArrays.js
+++ src/helpers/checkArrays.js
@@ -3,7 +3,13 @@ export const checkArrays = (keys, res) => {
 
   if (keys.length === res.evacuation.shortcode.split('+').length) {
     keys.sort().map((key, index) => {
-      if (key !== res.evacuation.shortcode.split('+').sort()[index]) {
+      if (
+        key.toLowerCase() !==
+        res.evacuation.shortcode
+          .toLowerCase()
+          .split('+')
+          .sort()[index]
+      ) {
         equal = false;
       } else {
         equal = true;
