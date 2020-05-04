c0e6f61c61ca8def317d2980c0bed74527795366
Jakub Biesiada <biesiadajakub@icloud.com>
2020-05-04 15:04:38 +0200

    feat: disable open in private window for unsupported protocols & provate windows

--- src/popup/App.vue
+++ src/popup/App.vue
@@ -2,23 +2,34 @@
   <div class="popup">
     <header class="popup__header">
       <h1>{{ name }}</h1>
-      <span>v{{ version }}</span>
+      <span>v{{ version }} beta 1</span>
     </header>
 
     <main class="popup__content">
       <button @click="openSettings">{{ settingsTitle }}</button>
-      <button @click="openInPrivate">{{ inPrivateTitle }}</button>
+      <button
+        @click="openInPrivate"
+        :disabled="isPrivateWindow || !isSupportedProtocol"
+      >
+        {{ inPrivateTitle }}
+      </button>
     </main>
   </div>
 </template>
 
 <script lang="ts">
+import { ref, onMounted } from 'vue';
 import extension from 'extensionizer';
 
 import { name, version } from '../manifest.json';
 
 export default {
   setup() {
+    const supportedProtocols: any = ['http', 'https'];
+
+    const isPrivateWindow = ref(false);
+    const isSupportedProtocol = ref(false);
+
     const openSettings = () => extension.runtime.openOptionsPage();
 
     const openInPrivate = () => {
@@ -36,11 +47,27 @@ export default {
       );
     };
 
+    onMounted(() => {
+      extension.windows.getCurrent(({ incognito, ...rest }: any) => {
+        isPrivateWindow.value = incognito;
+      });
+
+      extension.tabs.query({ active: true }, (tab: any) => {
+        // IMPORTANT TAB RETURNS ACTIVE TAB FOR EACH WINDOW
+
+        isSupportedProtocol.value = supportedProtocols.includes(
+          tab[0].url.split(':')[0]
+        );
+      });
+    });
+
     return {
       name,
       version,
       settingsTitle: 'Settings',
       inPrivateTitle: 'Open in private window',
+      isPrivateWindow,
+      isSupportedProtocol,
       openSettings,
       openInPrivate,
     };
